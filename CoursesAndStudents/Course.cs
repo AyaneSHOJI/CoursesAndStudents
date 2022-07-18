395bcd03ea9c65ae8b6f04b1a2bdcdf365f092e8
AyaneSHOJI <shojiayane@gmail.com>
2022-07-18 15:46:20 +0200

    created Course class

--- CoursesAndStudents/Course.cs
+++ CoursesAndStudents/Course.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
-namespace CoursesAndStudents
+namespace CoursesAndStudents;
+
+public class Course
 {
-    internal class Course
-    {
-    }
+    public int CourseId { get; set; }
+
+    [Required]
+    [StringLength(60)]
+    public string? Title { get; set; } 
+
+    public ICollection<Student> Students { get; set; }
 }
